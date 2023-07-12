76019531dadaaf38f5c7ae44867f7ba629784f3b
Munthera <alaesmohammad88@gmail.com>
2023-07-12 17:02:17 +0300

    new

---
+++ tasks-12-7/munther.js
@@ -0,0 +1,85 @@
+// // EX_1 
+// function tellFortune( nmchildren ,name ,location ,job){
+//           console.log( "You will be a " + job + "in" ,+ location+ "and a married to"+ name + "with" + nmchildren + "kids.");
+// }
+// tellFortune(5 ,"ar" , "aqaba" ,"programing" );
+// tellFortune(8 , "mu" , "aqaba" , "programing");
+//---------------------------------------------
+//EX_2
+//  function calculateDogAge(x){
+
+//     console.log("Your doggie is" + x * 7 + " years old in dog years!");
+//  }
+
+//  calculateDogAge(2);
+//------------------------------------------
+//EX_3
+// function calculateSupply( t , m){
+//     let day = (100-t)*365*m;
+
+//     console.log("You will need " + day + " to last you until the ripe old age of" +100);
+// }
+
+// calculateSupply(19,4);
+//------------------------------------------
+//EX_4
+// function greet(name){
+//     console.log('"hello munther"');
+// }
+// greet();
+//------------------------------------------
+//EX_5
+//what is the error:
+// function double(r){
+//     return 2*r;
+// }
+// function double(8){
+//     return 2*8;
+// }
+// function double('8'){
+// return 2*'8';
+// }
+//-------------------------------------------
+//EX_6
+// function double1 (x){
+//     return 2 * x ;
+//   }
+
+//   function double2 (x){
+//      return 2 * x;
+//   }
+//   function  double3 (x){
+//     return 2 * x;
+//   }
+//----------------------------------
+//Ex-7
+// function cube(p){
+//     console.log(p**3);
+// }
+// cube(8);
+//Ex-8
+// function multiply(m , u){
+//     console.log(m*u);
+// }
+//  multiply(7,9);
+//  multiply(5,9);
+//EX_9
+// function canIGetADrivingLicense(h){
+//  if(h>20)
+//     console.log('yes you can');
+
+//    else
+//       console.log('no you can');
+
+// }
+// canIGetADrivingLicense(18);
+//EX_10
+
+// function sameLength(ar,mu){
+//     if(ar.length == mu.length)
+//      console.log(true)
+//         else
+//           console.log(false); 
+// }
+// sameLength("tree","blue")
+// sameLength("tree","red")
